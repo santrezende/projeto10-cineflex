5bd310e97ae19d3cdde466e421878684001031af
santiago <santiagorsilva99@gmail.com>
2023-03-09 16:29:04 -0300

    mapeamento dos filmes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+import HomePage from "./pages/HomePage/HomePage"
+import SeatsPage from "./pages/SeatsPage/SeatsPage"
+import SessionsPage from "./pages/SessionsPage/SessionsPage"
+import SuccessPage from "./pages/SuccessPage/SuccessPage"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <>
+            <NavContainer>CINEFLEX</NavContainer>
+
+            <HomePage />
+            {/* <SeatsPage /> */}
+            {/*<SessionsPage /> */}
+            {/* <SuccessPage /> */}
+        </>
+    )
 }
 
-export default App;
+const NavContainer = styled.div`
+    width: 100%;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #C3CFD9;
+    color: #E8833A;
+    font-family: 'Roboto', sans-serif;
+    font-size: 34px;
+    position: fixed;
+    top: 0;
+    a {
+        text-decoration: none;
+        color: #E8833A;
+    }
+`
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import React, { useEffect } from "react";
+import axios from "axios";
+
+export default function HomePage() {
+    const url = "https://mock-api.driven.com.br/api/v8/cineflex/movies";
+    const [movies, setMovies] = React.useState([]);
+
+    useEffect(() => {
+        const request = axios.get(url);
+
+        request.then(response => {
+            setMovies(response.data);
+        });
+        request.catch(erro => {
+            console.log(erro.response.data);
+        })
+    }, [])
+
+    return (
+        <PageContainer>
+            Selecione o filme
+
+            <ListContainer>
+                {movies.map(movie =>
+                    <MovieContainer key={movie.id}>
+                        <img src={movie.posterURL} alt="poster" />
+                    </MovieContainer>)}
+            </ListContainer>
+
+        </PageContainer>
+    )
+}
+
+const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: 'Roboto';
+    font-size: 24px;
+    text-align: center;
+    color: #293845;
+    margin-top: 30px;
+    padding-top: 70px;
+`
+const ListContainer = styled.div`
+    width: 330px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    padding: 10px;
+`
+const MovieContainer = styled.div`
+    width: 145px;
+    height: 210px;
+    box-shadow: 0px 2px 4px 2px #0000001A;
+    border-radius: 3px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    img {
+        width: 130px;
+        height: 190px;
+    }
+`
